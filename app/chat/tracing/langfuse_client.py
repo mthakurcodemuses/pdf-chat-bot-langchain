e305b78849e197ab857f800e1aea32588f0cd868
manishthakur <mt86techlearning@gmail.com>
2023-11-03 15:09:36 +0000

    Langfuse Integration

--- app/chat/tracing/langfuse_client.py
+++ app/chat/tracing/langfuse_client.py
@@ -2,6 +2,6 @@ import os
 from langfuse.client import Langfuse
 
 langfuse = Langfuse(
-            os.environ.get("LANGFUSE_API_KEY"),
-            os.environ.get("LANGFUSE_SECRET_KEY"),
+            public_key=os.environ.get("LANGFUSE_PUBLIC_KEY"),
+            secret_key=os.environ.get("LANGFUSE_SECRET_KEY"),
             debug=True)
